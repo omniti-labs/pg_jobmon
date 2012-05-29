11651030d6f293a7d3ed80337f60b29a406d459a
Keith Fiske <keith@keithf4.com>
2012-05-28 20:36:01 -0400

    Fixed testshow script

--- test/testshow.sql
+++ test/testshow.sql
@@ -4,5 +4,5 @@ SELECT * FROM jobmon.show_job('PG_JOBMON TEST GOOD JOB');
 SELECT * FROM jobmon.show_job_like('jobmon');
 SELECT * FROM jobmon.show_job_status('CRITICAL');
 SELECT * FROM jobmon.show_job_status('PG_JOBMON TEST BAD JOB', 'CRITICAL');
-SELECT * FROM jobmon.show_detail((SELECT job_id FROM jobmon.show_job('PG_JOBMON TEST GOOD JOB') ORDER BY job_id DESC LIMIT 1)::bigint);
+SELECT * FROM jobmon.show_detail((SELECT job_id FROM jobmon.show_job('PG_JOBMON TEST GOOD JOB', 1) ORDER BY job_id DESC)::bigint);
 SELECT * FROM jobmon.show_detail('PG_JOBMON TEST GOOD JOB custom');
